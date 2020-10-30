edc5639b573253c5328a3565bc6f175bfc1425e6
Schulz Tobias <tsz.springe@gmail.com>
2020-10-30 13:19:45 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import {useEffect, useState} from "react";
+import getCharacters from "./services/getCharacters";
+
 
 export default function App() {
+
+  const [characters,setCharacters] = useState([])
+
+  useEffect(() => {
+    getCharacters()
+    .then (data => setCharacters(data.results))
+    .catch(error => console.log(error))
+  },[])
+
   return (
     <div className="App">
-      Hello World
+      <ul>
+      {characters.map(characters => <li>{characters.name}</li>)}
+      </ul>
+        
     </div>
   );
 }
