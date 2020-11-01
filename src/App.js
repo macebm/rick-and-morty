3789b1619aaf46a71421acf6648e42a690caa70b
Kurlyandski Artur <a.kurlyandski@hotmail.com>
2020-11-01 20:23:32 +0100

    added namesearch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Character from "./Character";
 import Header from "./Header";
 import getCharacters from "./services/getCharacters";
+import getCharactersByName from "./services/getCharactersByName";
 
 export default function App() {
 
@@ -17,22 +18,35 @@ export default function App() {
     
     <div className="App">
       <Header/>
-      <form onSubmit={logName}>
+      <form /*onSubmit={logName}*/>
         <label>
-          Search:
+          Search Name:
         </label><br />
-        <input type="text" name="searchName" />
+  <input type="text" name="searchName" onKeyUp={logName1} />
       </form>
-        {characters.map(({name, image, species, id}) => <Character key={id} name={name} imgUrl={image} species={species}/>)}
+         {typeof characters !== "undefined" &&
+          characters.map(({name, image, species, id}) => <Character key={id} name={name} imgUrl={image} species={species}/>)
+         }
     </div>
   );
 
-  function logName(event) {
+  // function logName(event) {
+  //   event.preventDefault()
+  //   const search = event.target
+  //   const searchedName = search.searchName
+  //   console.log(searchedName.value)
+  //   getCharactersByName(searchedName.value)
+  //   .then(namedata => setCharacters(namedata.results))
+  //   .catch(error => console.log(error))
+  // }
+
+  function logName1(event) {
     event.preventDefault()
     const search = event.target
-    const searchedName = search.searchName
-    console.log(searchedName.value)
-  
+    console.log(search.value)
+    getCharactersByName(search.value)
+    .then(namedata => setCharacters(namedata.results))
+    .catch(err => console.log(err))
   }
 }
 
--- src/services/getCharactersByName.js
+++ src/services/getCharactersByName.js
@@ -1,5 +1,5 @@
 
-export default function getOneCharacter(searchedName){
+export default function getCharactersByName(searchedName){
     return fetch("https://rickandmortyapi.com/api/character/?name=" + searchedName)
     .then(res => res.json())
     .catch(err => console.log(err))
