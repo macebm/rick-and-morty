f319b217964a8dfb842df4ba814c014e66ac2f05
macebm <marcelbauerat@gmail.com>
2020-11-04 11:26:33 +0100

    Add seatchCaracter logic

--- src/Characters.js
+++ src/Characters.js
@@ -6,33 +6,46 @@ import getSearchedCharacters from "./services/getSearchedCharacter";
 
 export default function Characters() {
   const [characters, setCharacters] = useState([]);
+  const [searchedCharacter, setSearchedCharacter] = useState([]);
+
+  function handleCharacterSearch(event) {
+    setSearchedCharacter(event.target.value);
+    if (event.key === "Enter") {
+      const searchedCharacter = event.target.value;
+      getSearchedCharacters(searchedCharacter)
+        .then((data) => setCharacters(data.results))
+        .catch((error) => console.log(error.message));
+    }
+  }
 
   useEffect(() => {
     getCharacters()
       .then((data) => setCharacters(data.results))
       .catch((error) => console.log(error.message));
-  }, []);
+  }, [searchedCharacter]);
 
   return (
     <ContainerSection>
-      <input
-        type="text"
-        placeholder="Write character name"
-        onKeyDown={(event) => {
-          if (event.key === "Enter") {
-            const searchedCharacter = event.target.value;
-            getSearchedCharacters(searchedCharacter)
-              .then((data) => setCharacters(data.results))
-              .catch((error) => console.log(error.message));
-          }
-        }}
-      />
+      <div>
+        <input
+          type="text"
+          placeholder="Write character name"
+          onKeyDown={handleCharacterSearch}
+          value={searchedCharacter}
+          onChange={handleCharacterSearch}
+        />
+        {searchedCharacter !== "" && (
+          <span onClick={() => setSearchedCharacter("")}>&times;</span>
+        )}
+      </div>
 
-      {characters !== undefined
-        ? characters.map(({ name, image, species, id }) => (
-            <Character key={id} name={name} imgUrl={image} species={species} />
-          ))
-        : alert("Not a character")}
+      {characters ? (
+        characters.map(({ name, image, species, id }) => (
+          <Character key={id} name={name} imgUrl={image} species={species} />
+        ))
+      ) : (
+        <h3>No character found with this name</h3>
+      )}
 
       {/* {characters.map(({ name, image, species, id }) => (
         <Character key={id} name={name} imgUrl={image} species={species} />
