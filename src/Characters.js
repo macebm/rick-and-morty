b72fe476dbd703b4e79ab7f2f5c865b65e1cd7c1
macebm <marcelbauerat@gmail.com>
2020-11-04 15:31:26 +0100

    Finished caracter pages logic/ Finished return if searched caracter doesnt exist

--- src/Characters.js
+++ src/Characters.js
@@ -3,11 +3,13 @@ import styled from "styled-components";
 import Character from "./Character";
 import ContainerSection from "./ContainerSection";
 import getCharacters from "./services/getCharacters";
+import getNextPageCharacters from "./services/getNextPageCharacters";
 import getSearchedCharacters from "./services/getSearchedCharacter";
 
 export default function Characters() {
   const [characters, setCharacters] = useState([]);
   const [searchedCharacter, setSearchedCharacter] = useState([]);
+  const [pageNumber, setPageNumber] = useState([1]);
 
   function handleCharacterSearch(event) {
     setSearchedCharacter(event.target.value);
@@ -19,11 +21,51 @@ export default function Characters() {
     }
   }
 
+  // function handlePageOfCharacters(operator) {
+  //   const count = Number(pageNumber);
+  //   if (operator === "-" && count > 1) {
+  //     const counter = count - 1;
+  //     setPageNumber(counter);
+  //   } else if (operator === "+" && count < 20) {
+  //     const counter = count + 1;
+  //     setPageNumber(counter);
+  //   } else {
+  //     alert("There are only pages between 1 and 20");
+  //   }
+  // }
+
+  function handlePageOfCharacters(operator) {
+    const count = Number(pageNumber);
+    if (operator === "-" && count > 1) {
+      const counter = count - 1;
+      setPageNumber(counter);
+    } else if (operator === "-" && count === 1) {
+      setPageNumber(20);
+    } else if (operator === "+" && count < 20) {
+      const counter = count + 1;
+      setPageNumber(counter);
+    } else if (operator === "+" && count === 20) {
+      setPageNumber(1);
+    }
+  }
+
+  useEffect(() => {
+    getNextPageCharacters(pageNumber)
+      .then((data) => setCharacters(data.results))
+      .catch((error) => console.log(error.message));
+  }, [pageNumber]);
+
   useEffect(() => {
     getCharacters()
       .then((data) => setCharacters(data.results))
       .catch((error) => console.log(error.message));
   }, [searchedCharacter]);
+
+  useEffect(() => {
+    getCharacters()
+      .then((data) => setCharacters(data.results))
+      .catch((error) => console.log(error.message));
+  }, []);
 
   return (
     <ContainerSection>
@@ -39,21 +81,58 @@ export default function Characters() {
           <span onClick={() => setSearchedCharacter("")}>&times;</span>
         )}
       </CaracterSearchBox>
+      <Position>
+        <button
+          onClick={() => {
+            handlePageOfCharacters("-");
+          }}
+        >
+          Back
+        </button>
+        <h3>{pageNumber} of 20</h3>
+        <button
+          onClick={() => {
+            handlePageOfCharacters("+");
+          }}
+        >
+          Next
+        </button>
+      </Position>
 
       {characters ? (
         characters.map(({ name, image, species, id }) => (
           <Character key={id} name={name} imgUrl={image} species={species} />
         ))
       ) : (
-        <h3>No character found with this name</h3>
+        <NoCaracter>
+          <h3>No character found with this name.</h3>
+          <h3>Click on the button below to return!</h3>
+          <button onClick={() => setSearchedCharacter("")}>RETURN</button>
+        </NoCaracter>
       )}
-
-      {/* {characters.map(({ name, image, species, id }) => (
-        <Character key={id} name={name} imgUrl={image} species={species} />
-      ))} */}
     </ContainerSection>
   );
 }
+
+const Position = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 20px;
+  width: 300px;
+  text-align: center;
+  margin: 0 auto 30px auto;
+  height: auto;
+
+  button {
+    border: none;
+    background: #1f2151;
+    border-radius: 6px;
+    padding: 1rem 2rem;
+    color: #f7f53f;
+    height: auto;
+  }
+`;
 
 const CaracterSearchBox = styled.div`
   border: 1px solid black;
@@ -71,5 +150,19 @@ const CaracterSearchBox = styled.div`
   span {
     color: red;
     margin-left: 5px;
+  }
+`;
+
+const NoCaracter = styled.div`
+  text-align: center;
+  color: red;
+
+  button {
+    border: none;
+    background: #1f2151;
+    border-radius: 6px;
+    padding: 1rem 2rem;
+    color: #f7f53f;
+    height: auto;
   }
 `;
--- src/services/getCharacters.js
+++ src/services/getCharacters.js
@@ -1,5 +1,5 @@
-export default function getCharacters(){
-    return fetch("https://rickandmortyapi.com/api/character/")
-    .then(res => res.json())
-    .catch(err => console.log(err.message))
+export default function getCharacters() {
+  return fetch("https://rickandmortyapi.com/api/character/")
+    .then((res) => res.json())
+    .catch((err) => console.log(err.message));
 }
